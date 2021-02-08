5cd1938e66fb63f9ac13128024a5159a45919599
aRzKailio <alan.quentin@orange.fr>
2021-02-08 09:38:44 +0100

    dploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:alanquentin85/vue-app-2.git master:ghpages
+git push -f git@github.com:alanquentin85/vue-app-2.git master:gh-pages
 
 cd -
